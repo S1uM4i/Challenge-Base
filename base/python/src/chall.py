608f2e5b6391c29d75eca33bad651714c1c02ebc
GZTime <Time.GZ@outlook.com>
2025-02-13 23:58:55 +0800

    chore: update chall.py

--- base/python/src/chall.py
+++ base/python/src/chall.py
@@ -2,7 +2,11 @@ import os
 
 if os.path.exists("/home/ctf/flag"):
     with open("/home/ctf/flag", "r") as f:
-        print(f.read())
-else:
-    print("Flag not found")
-    exit(1)
+        flag = f.read().strip()
+
+    if flag:
+        print(f"Flag: {flag}")
+        exit(0)
+
+print("There is no flag here")
+exit(1)
